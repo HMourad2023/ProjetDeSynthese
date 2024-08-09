ba87e3145e54deb68d058d8a79d380543ca5fd4b
mourad <hamzaoui.mourad@outlook.com>
2024-08-09 10:10:48 -0400

    Add Google Drive access workflow

---
+++ auth.py
@@ -0,0 +1,27 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+import os
+import json
+
+# Chargement des credentials depuis la variable d'environnement
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
+if GOOGLE_APPLICATION_CREDENTIALS is None:
+    raise ValueError("GOOGLE_APPLICATION_CREDENTIALS environment variable is not set")
+
+credentials_info = json.loads(GOOGLE_APPLICATION_CREDENTIALS)
+credentials = service_account.Credentials.from_service_account_info(
+    credentials_info, scopes=['https://www.googleapis.com/auth/drive.file'])
+
+service = build('drive', 'v3', credentials=credentials)
+
+# Liste des fichiers dans Google Drive
+results = service.files().list().execute()
+files = results.get('files', [])
+
+if not files:
+    print('No files found.')
+else:
+    print('Files:')
+    for file in files:
+        print(f"{file.get('name')} ({file.get('id')})")
