99691273f7425ce3e43b7e7b0b72aa27f9abd542
mourad <hamzaoui.mourad@outlook.com>
2024-08-09 12:48:05 -0400

    klkl

--- auth.py
+++ auth.py
@@ -2,63 +2,26 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import os
 import json
-import logging
 
-# Configure logging
-logging.basicConfig(level=logging.DEBUG)
+# Chemin du fichier credentials
+credentials_path = r'C:\Users\Asus_M\Desktop\credentials.json'
 
-# Chemin vers le fichier JSON de clé de service
-credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-
-if credentials_path is None:
-    raise ValueError("GOOGLE_APPLICATION_CREDENTIALS environment variable is not set")
-
-# Nettoyer les guillemets supplémentaires si nécessaire
-credentials_path = credentials_path.strip('"')
-
-# Charger les informations d'identification depuis le fichier JSON
+# Charger les credentials
 try:
     with open(credentials_path, 'r') as file:
         credentials_info = json.load(file)
-except (IOError, json.JSONDecodeError) as e:
+except OSError as e:
     raise ValueError(f"Error loading credentials file: {e}")
 
-# Préparer les informations d'identification pour le compte de service
+# Créer les credentials
 try:
-    creds = service_account.Credentials.from_service_account_info(
-        credentials_info,
-        scopes=['https://www.googleapis.com/auth/drive']  # Accès complet
-    )
-    logging.info("Credentials created successfully.")
-except Exception as e:
+    creds = service_account.Credentials.from_service_account_info(credentials_info)
+except ValueError as e:
     raise ValueError(f"Error creating credentials: {e}")
 
-# Construire le service Google Drive
-try:
-    service = build('drive', 'v3', credentials=creds)
-    logging.info("Google Drive service created successfully.")
-except Exception as e:
-    raise ValueError(f"Error building Google Drive service: {e}")
+# Créer le service Google Drive
+service = build('drive', 'v3', credentials=creds)
 
-# Lister les fichiers dans Google Drive
-try:
-    results = service.files().list(
-        pageSize=10,
-        fields="nextPageToken, files(id, name)"
-    ).execute()
-    logging.debug(f"Results: {results}")
-    files = results.get('files', [])
-    logging.info("Files retrieved successfully.")
-except Exception as e:
-    logging.error(f"An error occurred: {e}")
-    files = []
-
-if not files:
-    print('No files found.')
-else:
-    print('Files:')
-    for file in files:
-        print(f"{file.get('name')} ({file.get('id')})")
 
 
     
